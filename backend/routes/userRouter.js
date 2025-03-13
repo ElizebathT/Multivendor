c618252a5bfef19669ce109b2a575af50d6bdc33
Elizebath <elizebaththomasv@gmail.com>
2025-03-13 15:54:06 +0530

    chng password

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -6,6 +6,7 @@ const userRoutes = express.Router();
 userRoutes.post("/register", userController.register);
 userRoutes.post("/login", userController.login);
 userRoutes.put("/edit", userAuthentication,userController.profile);
+userRoutes.put("/password", userAuthentication,userController.changePassword);
 userRoutes.delete("/logout", userController.logout);
 userRoutes.get("/view", userAuthentication,userController.getUserProfile);
 
