079d5d2e9e989840d56e3cd9388ff4bc6bfe5318
Elizebath <elizebaththomasv@gmail.com>
2025-03-21 09:32:55 +0530

    Added chat

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,17 +8,23 @@ const orderRouter = require("./orderRoutes");
 const wishlistRoutes = require("./wishlistRoutes");
 const paymentRoutes = require("./paymentRoutes");
 const reviewRouter = require("./reviewRoutes");
+const complaintRouter = require("./complaintRoutes");
+const notifyRouter = require("./notificationRoutes");
+const chatRoutes = require("./chatRouter");
 const router=express()
 
 router.use("/payment", paymentRoutes);
 
 router.use(express.json())
-
+router.use("/complaint", complaintRouter);
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
+router.use("/notification", notifyRouter);
 router.use("/vendor", vendorRoutes);
 router.use("/admin", adminRoutes);
 router.use("/cart", cartRouter);
+router.use("/complaint", complaintRouter);
+router.use("/chat", chatRoutes);
 router.use("/review", reviewRouter);
 router.use("/order", orderRouter);
 router.use("/wishlist", wishlistRoutes);
